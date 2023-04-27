9591008cee02e87e5664b81868c1529ca5855453
El-Mundo <superbluecat71@outlook.com>
2023-04-27 02:59:19 +0100

    Updated scatter map data

--- GIS/Plotly/filmmaker_map-mapbox.py
+++ GIS/Plotly/filmmaker_map-mapbox.py
@@ -1,4 +1,4 @@
-############### LEGACY (ABONDONED DUE TO POOR PERFORMANCE) ###############
+############### POOR PERFORMANCE WARNING ###############
 
 import plotly.express as px
 import pandas as pd
