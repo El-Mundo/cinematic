cc834da44605d81807a4f26cc2a37a262fcfef7a
El-Mundo <superbluecat71@outlook.com>
2023-04-30 22:25:37 +0100

    Updated filmography visualisation

--- OCR/JAVA/romanizing/src/NameRoman.java
+++ OCR/JAVA/romanizing/src/NameRoman.java
@@ -16,13 +16,16 @@ public class NameRoman {
 	private final static String NONHAN_NAMES = "OCR/JAVA/romanizing/roman-non-han_names.csv";
 	private final static String ORGANIZATIONS = "OCR/JAVA/romanizing/translated-organizations.csv";
 	private final static String TAR = "OCR/translated-names.tsv", LIST_TAR = "OCR/names-full.csv";
+	private final static String NODES_LIST = "Network/csv/nodes/nodes-all.csv";
+
+	private final static boolean IGNORE_ORGANIZATIONS_IN_FULL_NAMES = true;
 
 	private static HashMap<String, String> jobToEng = new HashMap<String, String>();
 	private static HashMap<String, String> nonHanNameToEng = new HashMap<String, String>();
 	private static HashMap<String, String> organizations = new HashMap<String, String>();
 
 	public static void main(String[] args) throws IOException {
-		translateAllNames();
+		//translateAllNames();
 		writeAllNameAndTranslationList();
 	}
 
@@ -51,6 +54,16 @@ public class NameRoman {
 			reader2.close();
 			System.out.println("Finished reading indexes");
 		}
+		if(jobToEng.isEmpty()) {
+			File jobs = new File(JOBS);
+			BufferedReader reader3 = new BufferedReader(new FileReader(jobs));
+			String line = "";
+			while((line = reader3.readLine()) != null) {
+				String[] parts = line.split(",");
+				jobToEng.put(parts[0], parts[1]);
+			}
+			reader3.close();
+		}
 
 		int i = 0;
 
@@ -75,7 +88,6 @@ public class NameRoman {
 			}
 			for (String name : directing) {
 				String roman = "";
-				if(organizations.containsKey(name)) continue;
 				if(!nameToTranslated.containsKey(name)) {
 					if(organizations.containsKey(name)) {
 						roman = organizations.get(name);
@@ -115,17 +127,65 @@ public class NameRoman {
 		}
 		
 		BufferedWriter writer = new BufferedWriter(new FileWriter(LIST_TAR));
-		writer.append("key,full name");
+		writer.append("key,full name,main region,main job,main job (eng)\n");
 		i = 0;
 		for (String name : nameToTranslated.keySet()) {
-			writer.write(name + "," + name + "(" + nameToTranslated.get(name) + ")");
-			writer.newLine();
 			i++;
+			if(organizations.containsKey(name) && IGNORE_ORGANIZATIONS_IN_FULL_NAMES) continue;
+			String[] getCatAndRol = getMainGeoCatAndRole(name, organizations.containsKey(name));
+			writer.write(name + "," + name + "(" + nameToTranslated.get(name) + ")," + getCatAndRol[0] + "," + getCatAndRol[1] + "," + getCatAndRol[2]);
+			writer.newLine();
 			System.out.println("Wrote " + i + "/" + nameToTranslated.size());
 		}
 		writer.close();
 	}
 
+	private static String[] getMainGeoCatAndRole(String name, boolean isOrg) throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader(NODES_LIST));
+		String line = reader.readLine();
+		while((line = reader.readLine()) != null) {
+			String[] parts = line.split(",");
+			if(name.equals(parts[1].replaceAll("\"", ""))) {
+				String[] output = new String[3];
+				output[0] = parts[2].replaceAll("\"", "");
+				String[] jobs = parts[4].replaceAll("\"", "").split(" | ");
+				for (String string : jobs) {
+					string = string.trim();
+					if(string.equals("Acting")) {
+						output[1] = "演员";
+						output[2] = "actor/actress";
+						reader.close();
+						return output;
+					}else if(string.equals("Director")) {
+						output[1] = "导演";
+						output[2] = "director";
+						reader.close();
+						return output;
+					}else if(string.equals("Scriptwriter")) {
+						output[1] = "编剧";
+						output[2] = "scriptwriter";
+						reader.close();
+						return output;
+					}else{
+						output[1] = string;
+						if(jobToEng.containsKey(output[1])) {
+							output[2] = jobToEng.get(output[1]);
+						} else {reader.close(); throw new IOException("Job not found in jobs list: " + output[1]);}
+						reader.close();
+						return output;
+					}
+				}
+			}
+		}
+		reader.close();
+		if(isOrg) {
+			return new String[] {"", "", ""};
+		}else{
+			throw new IOException("Name not found in nodes list: " + name);
+		}
+	}
+
+	@SuppressWarnings("unused")
 	private static void translateAllNames() throws IOException {
 		File nonHanNames = new File(NONHAN_NAMES);
 		File jobs = new File(JOBS);
--- Visualiser/src/main.js
+++ Visualiser/src/main.js
@@ -13,16 +13,19 @@ var clicked = false, pClicked = false;
 
 //A out-of-frame slider to adjust the margin of the frame
 let margin_slider;
+let language_button;
+let search_field;
 
 //'chn' or 'eng'
 var lan = 'eng';
 
-//-1-loading data, 0-start menu, 1-catalogue, 2-map, 3-visualiser, 4-studio page, 5-film page
+//-1-loading data, 0-start menu, 1-catalogue, 2-map, 3-filmmakers, 4-studio page, 5-film page
 var state = -1, pState = -1;
 
 function preload() {
 	loadFonts();
 	loadData();
+	loadImages();
 }
 
 function setup() {
@@ -34,54 +37,97 @@ function setup() {
 	//Adapt display rate to the screen
 	frame_scale = parseFloat(windowHeight) / parseFloat(DEFAULT_HEIGHT);
 	let wantedWidth = (int)(DEFAULT_WIDTH * frame_scale);
-	createCanvas(wantedWidth * frame_margin_ratio, windowHeight * frame_margin_ratio);
+	cnv = createCanvas(wantedWidth * frame_margin_ratio, windowHeight * frame_margin_ratio);
 
 	//Init margin slider (html widget)
 	margin_slider = createSlider(0.75, 0.95, frame_margin_ratio, 0.025);
 	margin_slider.position(12, height);
 	margin_slider.style('width', '80px');
 	margin_slider.input(reset_margin_ratio);
+	//Init language button (html widget)
+	button = createButton('English/Chinese');
+	button.position(96, height);
+	button.mousePressed(switch_language);
+	//Init search field
+	search_field = createInput('');
+	search_field.size(width/4, height/24);
+	search_field.hide();
 
 	background(0);
 }
 
+function attempt_to_search_catalogue() {
+	search_in_catalogue(this.value);
+}
+
 function draw() {
+	background(0);
 	push();
-	scale(frame_scale * frame_margin_ratio);
-	translate(frame_offset_x, frame_offset_y);
-	textFont((lan === 'chn') ? font_chn : 'Arial');
-	switch (state) {
-		case -1:
-			draw_loading_scene();
-			if(loaded_resource >= RESOURCE_NUM) {
-				state = 1;
-				show_all_films_in_catalogue();
+		scale(frame_scale * frame_margin_ratio);
+		if(state == 1 || state == 5 || state == 3 || state == 4) {
+			draw_map_preview();
+		}
+		push();
+			translate(frame_offset_x, frame_offset_y);
+			textFont((lan === 'chn') ? font_chn : 'Arial');
+			switch (state) {
+				case -1:
+					draw_loading_scene();
+					if(loaded_resource >= RESOURCE_NUM) {
+						state = 1;
+						show_all_names_in_name_search();
+						show_all_names_in_studio_search();
+						start_search_map();
+						show_all_films_in_catalogue();
+					}
+					break;
+				case 1:
+					draw_catalogue();
+					break;
+				case 2:
+					draw_search_map();
+					break;
+				case 3:
+					draw_name_search();
+					break;
+				case 4:
+					draw_studio_page();
+					break;
+				case 5:
+					draw_film_page();
+					break;
+				default:
+					draw_start_menu();
+					break;
 			}
-			break;
-		case 1:
-			draw_catalogue();
-			break;
-		case 5:
-			draw_film_page();
-			break;
-		default:
-			draw_start_menu();
-			break;
-	}
+		pop();
+
+		if(state == 1) {
+			draw_bottom_bar();
+		}else if(state == 3) {
+			draw_name_preview();
+			draw_name_bottom_bar();
+		}else if(state == 4) {
+			draw_studio_preview();
+			draw_studio_bottom_bar();
+		}else if(state == 2) {
+			draw_map_bottom_bar();
+		}
+		
+		pCursorX = cursorX;
+		pcursorY = cursorY;
+		pClicked = clicked;
+		pState = state;
+		cursorX = (int)(parseFloat(mouseX) / frame_scale / frame_margin_ratio - frame_offset_x);
+		cursorY = (int)(parseFloat(mouseY) / frame_scale / frame_margin_ratio - frame_offset_y);
 	pop();
 
-	pCursorX = cursorX;
-	pcursorY = cursorY;
-	pClicked = clicked;
-	pState = state;
-	cursorX = (int)(parseFloat(mouseX) / frame_scale / frame_margin_ratio - frame_offset_x);
-	cursorY = (int)(parseFloat(mouseY) / frame_scale / frame_margin_ratio - frame_offset_y);
-
-	if(state == 5) {
-		fill(255);
-		rect(width-12, -64, 12, height+64);
-		fill(128);
-		rect(width-10, map(catalogue_scroll, 0, max_catalogue_scroll, 0, height-CATALOGUE_SLIDER_SIZE), 8, CATALOGUE_SLIDER_SIZE);
+	if(state == 1) {
+		draw_catalogue_scroll();
+	}else if(state == 3) {
+		draw_name_scroll();
+	}else if(state == 4) {
+		draw_studio_scroll();
 	}
 
 	fill(255);
@@ -94,22 +140,24 @@ function switch_language() {
 	} else if(lan == 'eng') {
 		lan = 'chn';
 	}
-	update_film_page();
+	if(state == 5)
+		update_film_page();
 }
 
 function keyPressed() {
-	if(keyCode == 80) {
+	/*if(keyCode == 76) {
 		switch_language();
 	}
 	if(keyCode == 38) {
 		catalogue_scroll += 50;
 	}else if(keyCode == 40) {
 		catalogue_scroll -= 100;
-	}
+	}*/
 }
 
 function mousePressed() {
-	clicked = true;
+	if(mouseX < width && mouseY < height)
+		clicked = true;
 }
 
 function mouseReleased() {
@@ -127,4 +175,31 @@ function windowResized() {
 	resizeCanvas(wantedWidth * frame_margin_ratio, windowHeight * frame_margin_ratio);
 
 	margin_slider.position(12, height);
+	button.position(96, height);
+	search_field.position(width/64, height-height/24*1.7);
+	search_field.size(width/4, height/24);
+}
+
+function mouseWheel(event) {
+	if(mouseX < width && mouseY < height) {
+		if(state == 1) {
+			if(max_catalogue_scroll > -DEFAULT_HEIGHT) return;
+			catalogue_scroll -= event.delta * 0.5;
+			if(catalogue_scroll > 0) catalogue_scroll = 0;
+			else if(catalogue_scroll < max_catalogue_scroll) catalogue_scroll = max_catalogue_scroll;
+			return false;
+		}else if(state == 3) {
+			if(max_names_scroll > -DEFAULT_HEIGHT) return;
+			names_scroll -= event.delta * 0.5;
+			if(names_scroll > 0) names_scroll = 0;
+			else if(names_scroll < max_names_scroll) names_scroll = max_names_scroll;
+			return false;
+		}else if(state == 4) {
+			if(max_studio_scroll > -DEFAULT_HEIGHT) return;
+			studio_scroll -= event.delta * 0.5;
+			if(studio_scroll > 0) studio_scroll = 0;
+			else if(studio_scroll < max_studio_scroll) studio_scroll = max_studio_scroll;
+			return false;
+		}
+	}
 }
