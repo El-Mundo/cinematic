81713a7075e535b2dfd896ad8c22015f798329be
El-Mundo <superbluecat71@outlook.com>
2023-05-06 14:51:10 +0100

    Added a topic summary

--- Topic/JAVA/lda/ResultDecoding.java
+++ Topic/JAVA/lda/ResultDecoding.java
@@ -9,6 +9,7 @@ import java.util.HashMap;
 
 import OCR.JAVA.Film;
 
+@SuppressWarnings("unused")
 public class ResultDecoding {
 	private static final String SRC = "Topic/results.txt";
 	private static final String TAR = "Topic/results_decoded.csv";
@@ -16,10 +17,16 @@ public class ResultDecoding {
 
 	public static void main(String[] args) {
 		//decodeRetuls();
-		rankMostFrequentTopicsInRegion();
+		//rankMostFrequentTopicsInRegion();
+		//writeDecodedResultsDeluxe();
+		rankMostFrequentTopicsInRegion("Shanghai (private)");
+		rankMostFrequentTopicsInRegion("Shanghai (state)");
+		rankMostFrequentTopicsInRegion("Northeast");
+		rankMostFrequentTopicsInRegion("Beijing");
+		rankMostFrequentTopicsInRegion("Xi'an");
+		rankMostFrequentTopicsInRegion("Canton");
 	}
 
-	@SuppressWarnings("unused")
 	private static void decodeRetuls() {
 		try {
 			ArrayList<String> decoded = new ArrayList<String>();
@@ -35,9 +42,33 @@ public class ResultDecoding {
 				String filmKey = line.substring(line.indexOf(FILM_KEY_FLAG) + FILM_KEY_FLAG.length(), line.indexOf(TOPIC_CODE_FLAG)).trim();
 				System.out.println(filmKey);
 				String topicCode = line.substring(line.indexOf(TOPIC_CODE_FLAG) + TOPIC_CODE_FLAG.length(), line.indexOf(TEXT_FLAG)).trim();
+
+				String[] allTopics = topicCode.split("\\), \\(");
+				HashMap<Integer, Double> topicMap = new HashMap<Integer, Double>();
+				for (String string : allTopics) {
+					if(string.startsWith("(")) {
+						string = string.substring(1);
+					}
+					if(string.endsWith(")")) {
+						string = string.substring(0, string.length() - 1);
+					}
+					int topic = Integer.parseInt(string.substring(0, string.indexOf(",")));
+					double prob = Double.parseDouble(string.substring(string.indexOf(",") + 1).trim());
+					topicMap.put(topic, prob);
+				}
+				Integer codeOfMostPossibleTopic = -1;
+				double maxProb = -1;
+
+				for (Integer topic : topicMap.keySet()) {
+					if (topicMap.get(topic) > maxProb) {
+						maxProb = topicMap.get(topic);
+						codeOfMostPossibleTopic = topic;
+					}
+				}
+
 				//System.out.println(topicCode);
-				int topic = Integer.parseInt(topicCode);
-				decoded.add(filmKey + "," + topic);
+				//int topic = Integer.parseInt(topicCode);
+				decoded.add(filmKey + "," + codeOfMostPossibleTopic);
 			}
 			br.close();
 
@@ -45,6 +76,52 @@ public class ResultDecoding {
 			BufferedWriter bw = new BufferedWriter(new java.io.FileWriter(TAR));
 			bw.write("Film Key,Topic\n");
 			for (String s : decoded) {
+				bw.write(s);
+				bw.newLine();
+			}
+			bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void writeDecodedResultsDeluxe() {
+		try {
+			HashMap<String, Integer> filmTopicMap = new HashMap<String, Integer>();
+			BufferedReader br = new BufferedReader(new FileReader(TAR));
+			String line = br.readLine();
+			while ((line = br.readLine()) != null) {
+				String filmKey = line.substring(0, line.indexOf(","));
+				int topic = Integer.parseInt(line.substring(line.indexOf(",") + 1));
+				if (!filmTopicMap.containsKey(filmKey)) {
+					filmTopicMap.put(filmKey, topic);
+				} else {
+					br.close();
+					throw new RuntimeException("Film key duplicated: " + filmKey);
+				}
+			}
+			br.close();
+
+			ArrayList<Film> films = Film.initAllFilms();
+			ArrayList<String> output = new ArrayList<String>();
+			for (Film film : films) {
+				if(!filmTopicMap.containsKey(film.key)) {
+					throw new RuntimeException("Film key not found in results: " + film.key);
+				}
+
+				int topic = filmTopicMap.get(film.key);
+				String production = film.productionToString();
+				String title = film.title;
+				String key = film.key;
+				String plot = film.plot;
+
+				output.add(key + "," + title + "," + film.year + "," + production + "," + film.getFilmType() + "," + topic + "," + plot);
+			}
+
+			//Write to file
+			BufferedWriter bw = new BufferedWriter(new java.io.FileWriter("Topic/results_full.csv"));
+			bw.write("Key,Film Title,Year,Production,Type,Topic,Plot Summary\n");
+			for (String s : output) {
 				bw.write(s);
 				bw.newLine();
 			}
@@ -94,7 +171,7 @@ public class ResultDecoding {
 			}
 
 			//Write to file
-			BufferedWriter bw = new BufferedWriter(new java.io.FileWriter("Topic/region_topic_count.csv"));
+			BufferedWriter bw = new BufferedWriter(new java.io.FileWriter("Topic/regions/region_topic_count(all).csv"));
 			bw.write("Region,Topic,Count\n");
 			for (String key : regionTopicCount.keySet()) {
 				bw.write(key + "," + regionTopicCount.get(key));
@@ -105,4 +182,61 @@ public class ResultDecoding {
 			e.printStackTrace();
 		}
 	}
+
+	private static void rankMostFrequentTopicsInRegion(String region) {
+		//Use a BufferedReader to read the results_decoded.csv and load film keys into a HashMap
+		try {
+			HashMap<String, Integer> filmTopicMap = new HashMap<String, Integer>();
+			BufferedReader br = new BufferedReader(new FileReader(TAR));
+			String line = br.readLine();
+			while ((line = br.readLine()) != null) {
+				String filmKey = line.substring(0, line.indexOf(","));
+				int topic = Integer.parseInt(line.substring(line.indexOf(",") + 1));
+				if (!filmTopicMap.containsKey(filmKey)) {
+					filmTopicMap.put(filmKey, topic);
+				} else {
+					br.close();
+					throw new RuntimeException("Film key duplicated: " + filmKey);
+				}
+			}
+			br.close();
+
+			ArrayList<Film> films = Film.initAllFilms();
+			HashMap<String, Integer> yearTopicCount = new HashMap<String, Integer>();
+			for (Film film : films) {
+				if(!filmTopicMap.containsKey(film.key)) {
+					throw new RuntimeException("Film key not found in results: " + film.key);
+				}
+
+				int topic = filmTopicMap.get(film.key);
+				String[] regions = film.getCategory();
+
+				for (String r : regions) {
+					if(!r.equals(region)) {
+						continue;
+					}
+
+					String key = film.year + "," + topic;
+
+					if (!yearTopicCount.containsKey(key)) {
+						yearTopicCount.put(key, 1);
+					} else {
+						yearTopicCount.put(key, yearTopicCount.get(key) + 1);
+					}
+				}
+			}
+
+			//Write to file
+			BufferedWriter bw = new BufferedWriter(new java.io.FileWriter("Topic/region_topic_count(" + region + ").csv"));
+			bw.write("Year,Topic,Count\n");
+			for (String key : yearTopicCount.keySet()) {
+				bw.write(key + "," + yearTopicCount.get(key));
+				bw.newLine();
+			}
+			bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
