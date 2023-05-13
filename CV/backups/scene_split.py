a90b68ea18014b1a82187ee8284f0bec6d50a91e
El-Mundo <superbluecat71@outlook.com>
2023-05-13 01:51:32 +0100

    Implemented thumbnail generation

--- CV/backups/scene_split.py
+++
@@ -1,6 +0,0 @@
-from scenedetect import detect, ContentDetector, split_video_ffmpeg
-
-VIDEO_FILE = 'CV/WomanBasketballNo5_clip.mp4'
-
-scene_list = detect(VIDEO_FILE, ContentDetector())
-split_video_ffmpeg(VIDEO_FILE, scene_list, show_progress=True)
